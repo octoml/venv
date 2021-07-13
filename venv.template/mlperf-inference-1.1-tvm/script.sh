de833083f6f648161f39477452818bc2d7ec4014
Grigori Fursin <gfursin@tmoreau-hbv3-00.yzxataka1cwe1ha2s5sqddqvqb.bx.internal.cloudapp.net>
2021-07-13 13:25:49 +0000

    added MLPerf inference r1.1 template (original and TVM)

---
+++ venv.template/mlperf-inference-1.1-tvm/script.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+#
+# Developer(s): 
+#  * Grigori Fursin, OctoML.ai
+#
+
+function ck_command {
+
+  echo "*************************************************************"
+  echo "$1"
+  echo ""
+
+  $1
+
+  if [ "${?}" != "0" ]; then 
+    exit 1; 
+  fi
+
+}
+
+ck_command "ck pull repo:ck-mlops"
+
+# Install packages to CK env entries
+ck_command "ck setup kernel --var.install_to_env=yes"
+
+ck_command "ck detect platform.os --platform_init_uoa=generic-linux-dummy"
+
+ck_command "ck detect soft:compiler.python --full_path=${CK_VENV_PYTHON_BIN}"
+
+ck_command "ck detect soft:compiler.gcc --full_path=`which gcc`"
+
+
+# Install pip packages
+ck_command "python3 -m pip install protobuf"
+
+
+ck_command "ck detect soft:tool.cmake"
+#ck_command "ck install package --quiet --tags=tool,cmake,src"
+
+ck_command "ck install package --quiet --tags=mlperf,inference,src,r1.1-tvm"
+
+ck_command "ck install package --tags=lib,python-package,absl"
+
+ck_command "ck install package --tags=lib,python-package,numpy"
+
+ck_command "ck install package --tags=lib,python-package,mlperf,loadgen"
+
+ck_command "ck install package --tags=lib,python-package,matplotlib"
+
+ck_command "ck install package --tags=lib,python-package,cython"
+
+ck_command "ck install package --tags=lib,python-package,opencv-python-headless"
+
+ck_command "ck install package --tags=tool,coco,api"
+
+ck_command "ck show env"
---
+++ venv.template/mlperf-inference-1.1/script.bat
@@ -0,0 +1,59 @@
+@echo off
+
+rem
+rem Developer(s): 
+rem  * Grigori Fursin, OctoML.ai
+rem
+
+call common.bat ck pull repo:ck-mlops
+if %errorlevel% neq 0 (exit /b 1)
+
+rem Install packages to CK env entries
+call common.bat ck setup kernel --var.install_to_env=yes
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck detect platform.os --platform_init_uoa=generic-linux-dummy
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck detect soft:compiler.python --full_path=${CK_VENV_PYTHON_BIN}
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck detect soft:compiler.gcc --full_path=`which gcc`
+if %errorlevel% neq 0 (exit /b 1)
+
+
+call common.bat python -m pip install protobuf
+if %errorlevel% neq 0 (exit /b 1)
+
+
+call common.bat ck detect soft:tool.cmake
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --quiet --tags=mlperf,inference,src,r1.1
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --tags=lib,python-package,absl
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --tags=lib,python-package,numpy
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --tags=lib,python-package,mlperf,loadgen
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --tags=lib,python-package,matplotlib
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --tags=lib,python-package,cython
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --tags=lib,python-package,opencv-python-headless
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck install package --tags=tool,coco,api
+if %errorlevel% neq 0 (exit /b 1)
+
+call common.bat ck show env
+if %errorlevel% neq 0 (exit /b 1)
+
+exit /b 0
